9e6a51afe58d555a18ed6533e0d220015c6bf4da
Gabriel Danis <danis.gabriel@fronius.com>
2025-04-14 10:19:41 +0200

    first little wc with handleEvent functionality but only js

---
+++ main.js
@@ -0,0 +1,32 @@
+customElements.define(
+    "wc-template",
+    class extends HTMLElement {
+        constructor() {
+            super();
+            this.btn = document.createElement("button");
+            this.btn.textContent = "hi there";
+            this.append(this.btn);
+            this.btn.addEventListener("click", this);
+            this.btn.addEventListener("mouseover", this);
+        }
+
+        handleEvent(event) {
+            this[`on${event.type}`](event);
+        }
+
+        onclick(event) {
+            console.log("you clicked bitch!");
+        }
+
+        onmouseover(event) {
+            console.log("you hovered bitch!");
+        }
+
+        connectedCallback() {
+            console.log("wc-template CONNECTED");
+        }
+        disconnectedCallback() {
+            console.log("wc-template DISCONNECTED");
+        }
+    }
+);
